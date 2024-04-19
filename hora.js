d3c7e08040409d7bf6d6685773911bc309aa5b14
AllanSzczpanski <165694160+AllanSzczpanski@users.noreply.github.com>
2024-04-19 15:56:18 -0300

    Resultado Imersão front-end Alura
    Não fiz a parte do JS

---
+++ hora.js
@@ -0,0 +1,9 @@
+//BOM DIA | BOA TARDE | BOA NOITE
+
+const greetingMessage =
+  currentHour >= 5 && currentHour < 12
+    ? "Bom dia"
+    : currentHour >= 12 && currentHour < 18
+    ? "Boa tarde"
+    : "Boa noite";
+
